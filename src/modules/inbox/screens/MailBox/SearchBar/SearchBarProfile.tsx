68707526a510e5fe18b5fe7591d326088961a399
Francisco Gerlison <franciscogerlison@ioasys.com.br>
2020-08-01 11:16:45 -0300

    feat: Add SearchBarProfile component

---
+++ src/modules/inbox/screens/MailBox/SearchBar/SearchBarProfile.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Pressable } from 'react-native';
+import styled from 'styled-components/native';
+import Animated, { Extrapolate } from 'react-native-reanimated';
+import { usePanGestureHandler } from 'react-native-redash';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
+import Text from '@core/Text';
+
+import { Styled } from '@core/types';
+import { useInterpolation } from '@helpers/hooks';
+
+const PROFILE_SIZE = 30;
+
+const SearchBarProfile = () => {
+  const {
+    gestureHandler,
+    translation: { y: translationY },
+    velocity,
+  } = usePanGestureHandler();
+  const translationInterpolation = useInterpolation(translationY);
+
+  const [selectedProfile, setSelectedProfile] = useState(2);
+
+  return (
+    <Pressable android_ripple={{ borderless: true, radius: 20 }}>
+      <PanGestureHandler {...gestureHandler}>
+        <S.Container>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <S.Profile
+              style={
+                index === selectedProfile
+                  ? { transform: [{ translateY: translationY }] }
+                  : {
+                      opacity: translationInterpolation(
+                        [-PROFILE_SIZE * 2, 0, PROFILE_SIZE * 2],
+                        [1, 0, 1],
+                      ),
+                      transform: [
+                        {
+                          scale: translationInterpolation(
+                            [-PROFILE_SIZE * 2, 0, PROFILE_SIZE * 2],
+                            [1, 0.4, 1],
+                            {
+                              extrapolate: Extrapolate.CLAMP,
+                            },
+                          ),
+                        },
+                      ],
+                    }
+              }
+            >
+              <Text color="WHITE">{index}</Text>
+            </S.Profile>
+          ))}
+        </S.Container>
+      </PanGestureHandler>
+    </Pressable>
+  );
+};
+
+const S = {
+  Container: styled(Animated.View)`
+    width: ${PROFILE_SIZE}px;
+    height: 100%;
+
+    justify-content: center;
+    align-items: center;
+
+    overflow: hidden;
+  `,
+  Profile: styled(Animated.View)<Styled>`
+    width: ${PROFILE_SIZE}px;
+    height: ${PROFILE_SIZE}px;
+
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 20px;
+    background-color: ${({ theme }) => theme.TERTIARY};
+
+    position: absolute;
+  `,
+};
+
+export default SearchBarProfile;
